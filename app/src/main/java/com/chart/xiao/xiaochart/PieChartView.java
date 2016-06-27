dfaa85d8d7697e037d134736f9fc27d22759c1af
cjxiao <13772521682@yeah.net>
2016-06-27 16:15:28 +0800

    list置为空，防止内存泄漏

--- app/src/main/java/com/chart/xiao/xiaochart/PieChartView.java
+++ app/src/main/java/com/chart/xiao/xiaochart/PieChartView.java
@@ -20,9 +20,16 @@ import java.util.List;
 
 public class PieChartView extends BaseView {
 
+    //注释的方块宽度
+    private static final int RECTF_WIDTH = 20;
+    //注释方块间的距离
+    private static final int RECTF_PADDING = 5;
+
     private List<Float> mPies = new ArrayList<Float>();
     //转换为百分比的数据
     private List<Float> mTempPies = new ArrayList<Float>();
+    //存储颜色的list
+    private List<Integer> mPieColors = new ArrayList<Integer>();
 
     public PieChartView(Context context) {
         super(context);
@@ -67,13 +74,28 @@ public class PieChartView extends BaseView {
             RectF rectF = new RectF(PADDING_LEHGTH_X, PADDING_LEHGTH_Y, PADDING_LEHGTH_X + 2 * radius, PADDING_LEHGTH_Y + 2 * radius);
             //一定要从-90°开始，不然会出现最后一个圆弧画不出来的bug
             float tempStart = -90;
+            int tempColor = 0;
             for (int i = 0; i < mTempPies.size(); i++){
-                tempPaint.setColor(Color.argb(255, (int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random())));
+                tempColor = Color.argb(255, (int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
+                mPieColors.add(tempColor);
+                tempPaint.setColor(tempColor);
                 float tempEnd = mTempPies.get(i);
                 canvas.drawArc(rectF, tempStart, tempEnd, true, tempPaint);
                 canvas.drawArc(rectF, tempStart, tempEnd, true, mPaint);
                 tempStart += tempEnd;
             }
+            Paint fontPaint = new Paint();
+            fontPaint.setStyle(Paint.Style.STROKE);
+            fontPaint.setAntiAlias(true);
+            fontPaint.setTextSize(16);
+            fontPaint.setColor(getResources().getColor(android.R.color.tertiary_text_dark));
+            for(int j = 0;j < mPieColors.size();j++){
+                tempPaint.setColor(mPieColors.get(j));
+                RectF colorRectF = new RectF(2 * (PADDING_LEHGTH_X + radius),PADDING_LEHGTH_Y+j*(RECTF_WIDTH+RECTF_PADDING),2 * (PADDING_LEHGTH_X + radius)+RECTF_WIDTH,PADDING_LEHGTH_Y+j*(RECTF_WIDTH+RECTF_PADDING)+RECTF_WIDTH);
+                canvas.drawRoundRect(colorRectF,3,3,tempPaint);
+                canvas.drawText(String.valueOf(5*mTempPies.get(j)/18).substring(0,5)+"%",2*(PADDING_LEHGTH_X + radius)+RECTF_WIDTH+RECTF_PADDING,PADDING_LEHGTH_Y+j*(RECTF_WIDTH+RECTF_PADDING)+fontPaint.getFontMetrics().descent-fontPaint.getFontMetrics().ascent,fontPaint);
+            }
         }
+        mPieColors = null;
     }
 }
